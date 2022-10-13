a68c964ace287478e17b292ed36ca23f55b49924
Yohan BENDAVID <ybendavi@e1r9p4.clusters.42paris.fr>
2022-10-13 05:42:31 +0200

    started1.5

---
+++ Module01/ex05/Harl.cpp
@@ -0,0 +1,31 @@
+#include "Harl.hpp"
+
+Harl::Harl(void)
+{
+
+}
+
+Harl::~Harl(void)
+{
+
+}
+
+void	Harl::debug(void)
+{
+
+}
+
+void	Harl::info(void)
+{
+
+}
+
+void	Harl::warning(void)
+{
+
+}
+
+void	Harl::error(void)
+{
+
+}
